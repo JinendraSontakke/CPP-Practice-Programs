4db8c1fc2ac968c8b881b0b96780041b3dbfe075
Jinendra Sontakke <63655965+sjinu17@users.noreply.github.com>
2021-05-01 22:00:34 +0530

    Add files via upload

---
+++ 1.first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"\n\n Print the welcome text in next line \n\n";
+    cout<<"--------------------------------------------\n";
+    cout<<"Welcome to \n";
+    cout<<"Codex";
+    return 0;
+}
